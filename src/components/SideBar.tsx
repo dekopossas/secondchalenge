3b370e51d0ad89b670e59bbf71b2cf4802fca213
DekoPossas <andrepossaster@gmail.com>
2022-05-30 13:03:05 -0300

    sidebar done

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,26 @@
-export function SideBar() {
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: any) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre: any) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
